e485b8e67491d269f833ef8bacd48adbed715fbd
aldencomida703 <aldencomida703@gmail.com>
2025-11-22 15:36:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const display=document.getElementById("display")
+let display = document.getElementById("display")
 
 function selectValue(val) {
-  if(display.value==="Error"||display.value==="Infinity"){
-    display.value="";
-  }
-    display.value += val;
-  }
-
-  function clearAll() {
+  if (display.value === "Error" || display.value === "Infinity") {
     display.value = "";
   }
+  display.value += val;
+}
 
-  function del() {
-    let current = display.value;
-    display.value = current.slice(0, -1);
-  }
+function clearAll() {
+  display.value = "";
+}
 
-  function calculate() {
-    
-    try {
-      let result = eval(display.value);
-      if (result===undefined){
-        display.value="Error";
-      }else{
-      display.value =result;}
-    } catch {
+function del() {
+  let current = display.value;
+  display.value = current.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let expr = display.value;
+    expr = expr.replace(/%/g, "/100");
+    let result = eval(expr);
+    if (result === undefined) {
       display.value = "Error";
+    } else {
+      display.value = result;
     }
+  } catch {
+    display.value = "Error";
   }
+}
